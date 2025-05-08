048fca75050ec3fb912d51ca988c489b3e276ef6
m.gonzalez.bitrian@changetheblock.com <m.gonzalez.bitrian@changetheblock.com>
2025-05-08 17:03:23 +0200

    Logo y CSSs

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core"
 import { RouterLink, RouterLinkActive } from "@angular/router"
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
 })
