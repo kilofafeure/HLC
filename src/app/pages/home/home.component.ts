17624703dd84d5baab26580109972ebf9212499e
m.gonzalez.bitrian@changetheblock.com <m.gonzalez.bitrian@changetheblock.com>
2025-05-12 09:08:38 +0200

    carousel working

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from "@angular/core"
 import { RouterLink } from "@angular/router"
+import { CarouselComponent } from "../../components/carousel/carousel.component"
+import { Slide } from "../../components/carousel/carousel.interface"
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CarouselComponent],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.css",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  slides: Slide[] = [
+    { headline: "", src: "../../../assets/carousel/home/home-1.jpeg" },
+    { headline: "", src: "../../../assets/carousel/home/home-2.jpeg" },
+    { headline: "", src: "../../../assets/carousel/home/home-3.jpeg" },
+    { headline: "", src: "../../../assets/carousel/home/home-4.jpeg" },
+    { headline: "", src: "../../../assets/carousel/home/home-5.jpeg" } ];
+}
