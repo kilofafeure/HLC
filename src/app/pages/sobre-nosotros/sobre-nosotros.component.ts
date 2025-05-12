e44e7afc703b6d27ddb5658cf28fb9bcf076fcf8
m.gonzalez.bitrian@changetheblock.com <m.gonzalez.bitrian@changetheblock.com>
2025-05-12 13:00:30 +0200

    comentando código / carousel sobre-nosotros

--- src/app/pages/sobre-nosotros/sobre-nosotros.component.ts
+++ src/app/pages/sobre-nosotros/sobre-nosotros.component.ts
@@ -1,10 +1,19 @@
 import { Component } from "@angular/core"
+import { CarouselComponent } from "../../components/carousel/carousel.component"
+import { Slide } from "../../components/carousel/carousel.interface"
 
 @Component({
   selector: "app-sobre-nosotros",
   standalone: true,
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: "./sobre-nosotros.component.html",
   styleUrl: "./sobre-nosotros.component.css",
 })
-export class SobreNosotrosComponent {}
+export class SobreNosotrosComponent {
+  slides: Slide[] = [
+    { headline: "", src: "../../../assets/carousel/sobre-nosotros/sobre-nosotros-1.jpeg" },
+    { headline: "", src: "../../../assets/carousel/sobre-nosotros/sobre-nosotros-2.jpeg" },
+    { headline: "", src: "../../../assets/carousel/sobre-nosotros/sobre-nosotros-3.jpeg" },
+    { headline: "", src: "../../../assets/carousel/sobre-nosotros/sobre-nosotros-4.jpeg" }
+  ];
+}
