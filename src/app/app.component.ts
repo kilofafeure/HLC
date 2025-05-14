9cb2bca8b248d0840ee1ec4dae1b16f04d697b78
m.gonzalez.bitrian@changetheblock.com <m.gonzalez.bitrian@changetheblock.com>
2025-05-14 19:14:19 +0200

    Vercel Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { RouterOutlet } from "@angular/router"
 import { HeaderComponent } from "./components/header/header.component"
 import { FooterComponent } from "./components/footer/footer.component"
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: "app-root",
@@ -10,6 +11,10 @@ import { FooterComponent } from "./components/footer/footer.component"
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Huerta La Curtia"
+
+  ngOnInit() {
+    inject();
+  }
 }
